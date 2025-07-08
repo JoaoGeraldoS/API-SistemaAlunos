530b30579c72911474c6384c9dd7a6a6da18e663
Joao Geraldo <117011183+JoaoGeraldoS@users.noreply.github.com>
2025-07-08 14:50:41 -0300

    Update AlunoRepository.java

--- src/main/java/com/talentos_banco/talentos_proz/alunos/repository/AlunoRepository.java
+++ src/main/java/com/talentos_banco/talentos_proz/alunos/repository/AlunoRepository.java
@@ -3,6 +3,8 @@ package com.talentos_banco.talentos_proz.alunos.repository;
 import com.talentos_banco.talentos_proz.alunos.dto.AlunoDTO;
 import com.talentos_banco.talentos_proz.alunos.model.AlunoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface AlunoRepository extends JpaRepository<AlunoModel, Long> {
+  Optional<AlunoModel> findBynome(String nome);
 }
